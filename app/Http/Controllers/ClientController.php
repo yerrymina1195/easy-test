a6a8b01821df53382d21c395de5ede09d4fed3a3
Makhan Diakho <makhandiakho95@gmail.com>
2023-09-20 19:23:05 +0000

    client crud finish

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\CreateClientRequest;
+use App\Http\Requests\UpdateClientRequest;
 use App\Models\Client;
 
 class ClientController extends Controller
@@ -20,6 +21,42 @@ class ClientController extends Controller
     $client = Client::create($data);
 
     return response()->json(['message' => 'Client créé avec succès'], 201);
+}
+public function show($id)
+{
+    $client = Client::find($id);
+
+    if (!$client) {
+        return response()->json(['message' => 'Client non trouvé'], 404);
+    }
+
+    return response()->json($client);
+}
+public function destroy($id)
+{
+    $client = Client::find($id);
+
+    if (!$client) {
+        return response()->json(['message' => 'Client non trouvé'], 404);
+    }
+
+    $client->delete();
+
+    return response()->json(['message' => 'Client supprimé avec succès']);
+}
+public function update(UpdateClientRequest $request, $id)
+{
+    $data = $request->validated();
+
+    $client = Client::find($id);
+
+    if (!$client) {
+        return response()->json(['message' => 'Client non trouvé'], 404);
+    }
+
+    $client->update($data);
+
+    return response()->json(['message' => 'Client mis à jour avec succès']);
 }
     //
 }
