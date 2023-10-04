0668ef4ee7988d67601d902a4bbc65bf3ba1043c
latyr0503 <latyr0503@gmail.com>
2023-10-04 14:18:42 +0000

    api pays

--- react/src/components/Test.jsx
+++ react/src/components/Test.jsx
@@ -1,26 +1,36 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Test = () => {
   const [countries, setCountries] = useState([]);
   const [selectedCountry, setSelectedCountry] = useState("");
 
   useEffect(() => {
-    fetch("https://restcountries.com/v3.1/all")
-      .then((response) => response.json())
-      .then((data) => {
-        setCountries(data);
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((response) => {
+        setCountries(response.data);
       })
       .catch((error) => {
-        console.error("Error fetching countries:", error);
+        console.error(
+          "Une erreur s'est produite lors de la requête API :",
+          error
+        );
       });
   }, []);
 
   const handleCountryChange = (event) => {
     setSelectedCountry(event.target.value);
   };
+
   return (
     <div className="mt-10">
-      <select value={selectedCountry} onChange={handleCountryChange}>
+      <label htmlFor="countrySelect">Sélectionnez un pays :</label>
+      <select
+        id="countrySelect"
+        value={selectedCountry}
+        onChange={handleCountryChange}
+      >
         <option value="">Sélectionnez un pays</option>
         {countries.map((country) => (
           <option key={country.name.common} value={country.name.common}>
@@ -28,6 +38,7 @@ const Test = () => {
           </option>
         ))}
       </select>
+      {selectedCountry && <p>Vous avez sélectionné : {selectedCountry}</p>}
     </div>
   );
 };
